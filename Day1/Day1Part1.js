86b7f42025f5373bbb5f7bed3c919812cdde148b
Austin Schwartz <schwartzmas52@gmail.com>
2020-02-07 23:29:05 -0600

    Completed Day1 Part1 and Added Async Module

---
+++ Day1/Day1Part1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const async = require('async');
+
+fs.readFile('./Day1Part1.txt', 'utf8', function(err, contents) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    const values = contents.split("\n");
+    var total = 0;
+
+    async.forEach(values, function(value, cb) {
+        if (value) {
+            total += (Math.floor(value / 3) - 2);
+        }
+        return cb(null)
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log(total);
+    });
+});
